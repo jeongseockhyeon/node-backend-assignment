caec49d547844122e4f9c34f85a8d0f83d481cf4
jsh <jeong9260@naver.com>
2023-04-11 23:06:48 +0900

    [ADD]Variables

--- JeongSeokHyeon/techit_node_backend/momentum/app.js
+++ JeongSeokHyeon/techit_node_backend/momentum/app.js
@@ -1,1 +1,13 @@
-alert('hi')
+// alert('hi')
+
+// console.log(545454)
+// console.log("alalala");
+const a = 5
+const b = 2
+const myName = 'jsh'
+
+console.log(a + b)
+console.log(a * b)
+console.log(a / b)
+
+console.log('hello' + myName)
