a309523055dc522dbff33fc10f5805898456d09a
jsh <jeong9260@naver.com>
2023-04-13 10:41:19 +0900

    [DEL]경로 수정

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -2,17 +2,20 @@
 
 // console.log(545454)
 // console.log("alalala");
-const a = 5
-const b = 2
+// const a = 5
+// const b = 2
 // const myName = 'jsh'
 
 // let a= 5
 // let b =2
-let myName = 'jsh'
+// let myName = 'jsh'
 
 // console.log(a + b)
 // console.log(a * b)
 // console.log(a / b)
-myName = 'jeongseekhyeon'
+// myName = 'jeongseekhyeon'
 // console.log('hello' + myName)
-console.log('your new name is' + myName)
+// console.log('your new name is' + myName)
+
+const amIFat = false
+console.log(amIFat)
