6fcf53933770b8d7451234241c27e16ce20cec0e
jsh <jeong9260@naver.com>
2023-04-13 22:25:32 +0900

    [ADD]#2.5Arrays 실습

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -20,9 +20,29 @@
 // const amIFat = false
 // console.log(amIFat)
 
-const amIFat = null
-let something
-console.log(amIFat)
+// const amIFat = null
+// let something
+// console.log(amIFat)
 //null
-console.log(something)
+// console.log(something)
 //undefined
+// const mon = 'mon'
+// const tue = 'tue'
+// const wed = 'wed'
+// const thu = 'thu'
+// const fri = 'fri'
+// const sat = 'sat'
+// const sun = 'sun'
+
+// const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun]
+// const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+// const nonsense = [1, 2, 'hello', false, null, true, undefined, 'jsh']
+
+// console.log(daysOfWeek, nonsense)
+// console.log(daysOfWeek[4])
+
+// daysOfWeek.push('sun')
+// console.log(daysOfWeek)
+
+const toBuy = ['potato', 'tomato', 'pizza']
+console.log(toBuy[4])
