26d018de970fe8c63f70e02cba18bd74fb524c4f
jsh <jeong9260@naver.com>
2023-04-19 05:22:28 +0900

    [ADD]3.8 classList,toggle 실습

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -292,13 +292,19 @@
 
 // h1.addEventListener('click', handleTitleClick)
 
+// const h1 = document.querySelector('div.hello:first-child h1')
+// const clickedClass = 'clicked'
+// function handleTitleClick() {
+//   if (h1.classList.contains(clickedClass)) {
+//     h1.classList.remove(clickedClass)
+//   } else {
+//     h1.classList.add(clickedClass)
+//   }
+// }
+// h1.addEventListener('click', handleTitleClick)
+
 const h1 = document.querySelector('div.hello:first-child h1')
-const clickedClass = 'clicked font'
 function handleTitleClick() {
-  if (h1.className === clickedClass) {
-    h1.className = ''
-  } else {
-    h1.className = clickedClass
-  }
+  h1.classList.toggle('clicked')
 }
 h1.addEventListener('click', handleTitleClick)
