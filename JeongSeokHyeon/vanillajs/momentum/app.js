348a2c087db1a3b8aabf4d4c51d17fd589e19d72
jsh <jeong9260@naver.com>
2023-04-18 10:35:16 +0900

    [ADD]3.6 조건문을 이용한 이벤트를 통해 style 변경

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -239,39 +239,54 @@
 
 // const h1 = document.getElementsByTagName('h1')
 // const h1 = document.getElementsByTagName('h1')
-const h1 = document.querySelector('div.hello:first-child h1')
+// const h1 = document.querySelector('div.hello:first-child h1')
 // h1.innerText = 'Hello'
 // console.log(h1)
 
 // h1.style.color = 'blue'
+// function handleTitleClick() {
+//   h1.style.color = 'blue'
+// }
+// function handleMouseEnter() {
+//   h1.innerText = 'Mouse is here'
+// }
+// function handleMouseLeave() {
+//   h1.innerText = 'Mouse is gone'
+// }
+// function handleWindowResize() {
+//   document.body.style.backgroundColor = 'tomato'
+// }
+
+// function handleWindowCopy() {
+//   alert('copier')
+// }
+// function handleWindowOffline() {
+//   alert('SOS  no wifi')
+// }
+// function handleWindowOnline() {
+//   alert('ALL GOOD')
+// }
+
+// h1.addEventListener('click', handleTitleClick)
+// h1.addEventListener('mouseenter', handleMouseEnter)
+// h1.addEventListener('mouseleave', handleMouseLeave)
+// h1.onClick = handleTitleClick
+
+// window.addEventListener('resize', handleWindowResize)
+// window.addEventListener('copy', handleWindowCopy)
+// window.addEventListener('offline', handleWindowOffline)
+// window.addEventListener('online', handleWindowOnline)
+const h1 = document.querySelector('div.hello:first-child h1')
+
 function handleTitleClick() {
-  h1.style.color = 'red'
-}
-function handleMouseEnter() {
-  h1.innerText = 'Mouse is here'
-}
-function handleMouseLeave() {
-  h1.innerText = 'Mouse is gone'
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = 'tomato'
-}
-function handleWindowCopy() {
-  alert('copier')
-}
-function handleWindowOffline() {
-  alert('SOS  no wifi')
-}
-function handleWindowOnline() {
-  alert('ALL GOOD')
+  const currentColor = h1.style.color
+  let newColor
+  if (currentColor === 'blue') {
+    newColor = 'tomato'
+  } else {
+    newColor = 'blue'
+  }
+  h1.style.color = newColor
 }
 
 h1.addEventListener('click', handleTitleClick)
-h1.addEventListener('mouseenter', handleMouseEnter)
-h1.addEventListener('mouseleave', handleMouseLeave)
-// h1.onClick = handleTitleClick
-
-window.addEventListener('resize', handleWindowResize)
-window.addEventListener('copy', handleWindowCopy)
-window.addEventListener('offline', handleWindowOffline)
-window.addEventListener('online', handleWindowOnline)
