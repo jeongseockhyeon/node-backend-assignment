37a618b4cf1672507c5c6987e0de30801f9c3f8c
jsh <jeong9260@naver.com>
2023-04-18 11:11:12 +0900

    [ADD]3.7 JS로 CSS 다루기 실습

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -276,17 +276,29 @@
 // window.addEventListener('copy', handleWindowCopy)
 // window.addEventListener('offline', handleWindowOffline)
 // window.addEventListener('online', handleWindowOnline)
+
+// const h1 = document.querySelector('div.hello:first-child h1')
+
+// function handleTitleClick() {
+//   const currentColor = h1.style.color
+//   let newColor
+//   if (currentColor === 'blue') {
+//     newColor = 'tomato'
+//   } else {
+//     newColor = 'blue'
+//   }
+//   h1.style.color = newColor
+// }
+
+// h1.addEventListener('click', handleTitleClick)
+
 const h1 = document.querySelector('div.hello:first-child h1')
-
+const clickedClass = 'clicked font'
 function handleTitleClick() {
-  const currentColor = h1.style.color
-  let newColor
-  if (currentColor === 'blue') {
-    newColor = 'tomato'
+  if (h1.className === clickedClass) {
+    h1.className = ''
   } else {
-    newColor = 'blue'
+    h1.className = clickedClass
   }
-  h1.style.color = newColor
 }
-
 h1.addEventListener('click', handleTitleClick)
