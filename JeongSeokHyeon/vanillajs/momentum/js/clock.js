a8141f5756213c35d060f1ab209f67827e802c08
jsh <jeong9260@naver.com>
2023-04-20 18:44:32 +0900

    [ADD]5.1 Timeount 실습 및 시계 구현

--- JeongSeokHyeon/vanillajs/momentum/js/clock.js
+++ JeongSeokHyeon/vanillajs/momentum/js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector('h2#clock')
 
-function sayHello() {
-  console.log('hello')
+function getClcok() {
+  const date = new Date()
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
-setInterval(sayHello, 5000)
+getClcok()
+setInterval(getClcok, 1000)
