145f528a56dd6446c4c8ee0e2d3f7a7342977838
jsh <jeong9260@naver.com>
2023-04-20 15:03:19 +0900

    [ADD]4.5 사용자 이름 값 가져오기

--- JeongSeokHyeon/vanillajs/momentum/app.js
+++ JeongSeokHyeon/vanillajs/momentum/app.js
@@ -1,14 +1,16 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDDEN_CLASSNAME = 'hidden'
 
 const link = document.querySelector('a')
 function onLoginSibmit(event) {
   event.preventDefault()
-  console.log(loginInput.value)
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-function handleLinkClick(event) {
-  event.preventDefault()
-  console.log(event)
-}
+
 loginForm.addEventListener('submit', onLoginSibmit)
-link.addEventListener('click', handleLinkClick)
