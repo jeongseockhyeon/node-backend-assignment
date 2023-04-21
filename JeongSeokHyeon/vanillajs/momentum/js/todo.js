ee57cb68f130e7dcb2157ad9de3ef8820095445a
jsh <jeong9260@naver.com>
2023-04-22 01:00:12 +0900

    [ADD]7.1 todo추가하기 구현

--- JeongSeokHyeon/vanillajs/momentum/js/todo.js
+++ JeongSeokHyeon/vanillajs/momentum/js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById('todo-form')
 const toDoinput = document.querySelector('#todo-form input')
 const toDoList = document.getElementById('todo-list')
 
+function paintToDo(newTodo) {
+  const li = document.createElement('li')
+  const span = document.createElement('span')
+  li.append(span)
+  span.innerText = newTodo
+  toDoList.appendChild(li)
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault()
   const newTodo = toDoinput.value
   toDoinput.value = ''
+  paintToDo(newTodo)
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit)
