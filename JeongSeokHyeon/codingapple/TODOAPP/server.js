1146352fe4b8175e01f4cdf15360390d3102c26a
jsh <jeong9260@naver.com>
2023-05-10 21:19:12 +0900

    [ADD] 홈 추가

--- JeongSeokHyeon/codingapple/TODOAPP/server.js
+++ JeongSeokHyeon/codingapple/TODOAPP/server.js
@@ -11,3 +11,6 @@ app.get('/pet', function (요청, 응답) {
 app.get('/beauty', function (요청, 응답) {
   응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다')
 })
+app.get('/', function (요청, 응답) {
+  응답.sendFile(__dirname + '/index.html')
+})
