5b0f015e18fb62141d2338323786a0c04a2d45c4
jsh <jeong9260@naver.com>
2023-05-12 12:21:56 +0900

    [ADD] mongoDB 연결

--- JeongSeokHyeon/codingapple/TODOAPP/server.js
+++ JeongSeokHyeon/codingapple/TODOAPP/server.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(8080, function () {
-  console.log('listening on 8080')
-})
+const MongoClient = require('mongodb').MongoClient
 
 app.get('/pet', function (요청, 응답) {
   응답.send('펫용품 쇼핑할 수 있는 페이지입니다')
@@ -24,3 +22,14 @@ app.post('/add', function (요청, 응답) {
   console.log(요청.body)
   응답.send('요청 전송 완료')
 })
+
+MongoClient.connect(
+  'mongodb+srv://jsh:jsh1357924605@cluster0.vavbgct.mongodb.net/?retryWrites=true&w=majority',
+  function (에러, client) {
+    if (에러) return console.log(에러)
+
+    app.listen(8080, function () {
+      console.log('listening on 8080')
+    })
+  }
+)
