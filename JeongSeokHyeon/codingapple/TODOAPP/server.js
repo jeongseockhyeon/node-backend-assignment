d11254fffdf0f989fdd4547d9e953b71300c4de5
jsh <jeong9260@naver.com>
2023-05-12 09:38:54 +0900

    [ADD] write 페이지 구현

--- JeongSeokHyeon/codingapple/TODOAPP/server.js
+++ JeongSeokHyeon/codingapple/TODOAPP/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(8080, function () {
   console.log('listening on 8080')
@@ -13,4 +16,11 @@ app.get('/beauty', function (요청, 응답) {
 })
 app.get('/', function (요청, 응답) {
   응답.sendFile(__dirname + '/index.html')
+})
+app.get('/write', function (요청, 응답) {
+  응답.sendFile(__dirname + '/write.html')
+})
+app.post('/add', function (요청, 응답) {
+  console.log(요청.body)
+  응답.send('요청 전송 완료')
 })
