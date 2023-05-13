2a72e661c0df62989320f2ea8c75f42fe703c43d
jsh <jeong9260@naver.com>
2023-05-13 14:56:18 +0900

    [ADD] DB 저장실습

--- JeongSeokHyeon/codingapple/TODOAPP/server.js
+++ JeongSeokHyeon/codingapple/TODOAPP/server.js
@@ -23,10 +23,21 @@ app.post('/add', function (요청, 응답) {
   응답.send('요청 전송 완료')
 })
 
+var db
+
 MongoClient.connect(
   'mongodb+srv://jsh:jsh1357924605@cluster0.vavbgct.mongodb.net/?retryWrites=true&w=majority',
+  { useUnifiedTopology: true },
   function (에러, client) {
     if (에러) return console.log(에러)
+    db = client.db('todoapp')
+
+    db.collection('post').insertOne(
+      { 이름: 'jeong', _id: 100 },
+      function (에러, 결과) {
+        console.log('저장완료')
+      }
+    )
 
     app.listen(8080, function () {
       console.log('listening on 8080')
