34f49ed1c5f313efeab77cc544b6a142de2153c5
jsh <jeong9260@naver.com>
2023-05-26 16:39:19 +0900

    [ADD]채팅 기능

--- JeongSeokHyeon/codingapple/TODOAPP/server.js
+++ JeongSeokHyeon/codingapple/TODOAPP/server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const { ObjectId } = require('mongodb')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use('/public', express.static('public'))
 app.use(methodOverride('_method'))
-
 app.set('view engine', 'ejs')
 
 const MongoClient = require('mongodb').MongoClient
@@ -257,4 +258,66 @@ app.post('/upload', upload.single('profile'), function (요청, 응답) {
 
 app.get('/image/:imageName', function (요청, 응답) {
   응답.sendFile(__dirname + '/public/image/' + 요청.params.imageName)
+})
+
+app.post('/chatroom', 로그인했니, function (요청, 응답) {
+  var 저장할거 = {
+    title: '무슨무슨채팅방',
+    member: [ObjectId(요청.body.당한사람id), 요청.user._id],
+    date: new Date(),
+  }
+
+  db.collection('chatroom')
+    .insertOne(저장할거)
+    .then((결과) => {})
+})
+app.get('/chat', 로그인했니, function (요청, 응답) {
+  db.collection('chatroom')
+    .find({ member: 요청.user._id })
+    .toArray()
+    .then((결과) => {
+      응답.render('chat.ejs', { data: 결과 })
+    })
+})
+
+app.post('/message', 로그인했니, function (요청, 응답) {
+  var 저장할거 = {
+    parent: 요청.body.parent,
+    content: 요청.body.content,
+    userid: 요청.user._id,
+    date: new Date(),
+  }
+  db.collection('message')
+    .insertOne(저장할거)
+    .then(() => {
+      console.log('DB저장성공')
+      응답.send('DB저장성공')
+    })
+})
+
+app.get('/message/:parentid', 로그인했니, function (요청, 응답) {
+  응답.writeHead(200, {
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+  })
+
+  db.collection('message')
+    .find({ parent: 요청.params.parentid })
+    .toArray()
+    .then((결과) => {
+      console.log(결과)
+      응답.write('event: test\n')
+      응답.write(`data: ${JSON.stringify(결과)}\n\n`)
+    })
+
+  const 찾을문서 = [{ $match: { 'fullDocument.parent': 요청.params.parentid } }]
+
+  const changeStream = db.collection('message').watch(찾을문서)
+  changeStream.on('change', (result) => {
+    console.log(result.fullDocument)
+    응답.write('event: test\n')
+    var 추가된문서 = [result.fullDocument]
+    응답.write(`data: ${JSON.stringify(추가된문서)}\n\n`)
+  })
 })
