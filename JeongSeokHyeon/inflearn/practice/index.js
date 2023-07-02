d2b4499b7f62572b2aa22162c1fa18aac1d6a4df
jsh <jeong9260@naver.com>
2023-07-02 19:50:53 +0900

    [ADD]readFile

--- JeongSeokHyeon/inflearn/practice/index.js
+++ JeongSeokHyeon/inflearn/practice/index.js
@@ -5,5 +5,8 @@
 // console.log(result)
 
 const fs = require('fs')
-const data = fs.readFileSync('./data.txt', 'utf-8')
-console.log(data)
+// const data = fs.readFileSync('./data.txt', 'utf-8')
+
+const data = fs.readFile('./data.txt', 'utf8', function (err, data) {
+  console.log(data)
+})
