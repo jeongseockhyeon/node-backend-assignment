1f1a49d0d66f37812bcc0efc755d99c71bb60d6e
jsh <jeong9260@naver.com>
2023-07-02 20:05:08 +0900

    [ADD]헬로월드

--- JeongSeokHyeon/inflearn/practice/index.js
+++ JeongSeokHyeon/inflearn/practice/index.js
@@ -4,9 +4,23 @@
 
 // console.log(result)
 
-const fs = require('fs')
+// const fs = require('fs')
 // const data = fs.readFileSync('./data.txt', 'utf-8')
 
-const data = fs.readFile('./data.txt', 'utf8', function (err, data) {
-  console.log(data)
+// const data = fs.readFile('./data.txt', 'utf8', function (err, data) {
+//   console.log(data)
+// })
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World!\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
