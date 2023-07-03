51fd22b250986613fc88a1fc26ddf421d30adb95
jsh <jeong9260@naver.com>
2023-07-03 21:29:42 +0900

    [ADD]npm

--- JeongSeokHyeon/inflearn/NODE-API/index.js
+++ JeongSeokHyeon/inflearn/NODE-API/index.js
@@ -1,20 +1,11 @@
 const express = require('express')
-const logger3 = require('morgan')
 const app = express()
+const port = 3000
 
-function logger(req, res, next) {
-  console.log('i am logger')
-  next()
-}
-function logger2(req, res, next) {
-  console.log('i am logger2')
-  next()
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use(logger)
-app.use(logger2)
-app.use(logger3('dev'))
-
-app.listen(3000, function () {
-  console.log('Servre is running')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
