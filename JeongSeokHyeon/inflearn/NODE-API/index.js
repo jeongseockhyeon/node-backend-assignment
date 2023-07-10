984935af76a07a56cdd1ea4a6252753577acfdd3
jsh <jeong9260@naver.com>
2023-07-10 15:13:39 +0900

    [ADD]테스트 환경 개선

--- JeongSeokHyeon/inflearn/NODE-API/index.js
+++ JeongSeokHyeon/inflearn/NODE-API/index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const user = require('./api/user')
 
-app.use(morgan('dev'))
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'))
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/users', user)
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 module.exports = app
