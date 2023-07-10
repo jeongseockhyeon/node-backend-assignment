6172316083ec3455afd5d4fdbd3bf7ee471d224c
jsh <jeong9260@naver.com>
2023-07-10 14:33:25 +0900

    [ADD]사용자 수정 API 성공시 + 실패시

--- JeongSeokHyeon/inflearn/NODE-API/index.spec.js
+++ JeongSeokHyeon/inflearn/NODE-API/index.spec.js
@@ -27,7 +27,7 @@ describe('GET /users는', () => {
     })
   })
 })
-describe('GET /users/1는', () => {
+describe('GET /users/:id', () => {
   describe('성공시', () => {
     it('id가 1인 유저 객체를 반환한다', (done) => {
       request(app)
@@ -47,7 +47,7 @@ describe('GET /users/1는', () => {
     })
   })
 })
-describe('DELETE /users/1', () => {
+describe('DELETE /users/:id', () => {
   describe('성공시', () => {
     it('204를 응답한다', (done) => {
       request(app).delete('/users/1').expect(204).end(done)
@@ -86,6 +86,36 @@ describe('POST /users', () => {
     })
     it('name이 중복일 경우 409를 반환한다', (done) => {
       request(app).post('/users').send({ name: 'daniel' }).expect(409).end(done)
+    })
+  })
+})
+describe('PUT /users:id', () => {
+  describe('성공시', () => {
+    it('변경된 name을 응답한다', (done) => {
+      const name = 'chally'
+      request(app)
+        .put('/users/3')
+        .send({ name })
+        .end((err, res) => {
+          res.body.should.have.property('name', name)
+          done()
+        })
+    })
+  })
+  describe('실패시', () => {
+    it('정수가 아닌 id일 경우 400을 응답한다', (done) => {
+      request(app).put('/users/one').expect(400).end(done)
+    })
+    it('name이 없을 경우 400을 응답한다', (done) => {
+      request(app).put('/users/1').send({}).expect(400).end(done)
+    })
+
+    it('없는 유저일 경우 404을 응답한다', (done) => {
+      request(app).put('/users/999').send({ name: 'foo' }).expect(404).end(done)
+    })
+
+    it('이름이 중복일 409을 응답한다', (done) => {
+      request(app).put('/users/3').send({ name: 'bek' }).expect(409).end(done)
     })
   })
 })
