bd12c5e6e68f234d7f241341a70236dd3edd94e8
jsh <jeong9260@naver.com>
2023-07-10 16:33:54 +0900

    [ADD]DB와 create컨트롤러 연동

--- JeongSeokHyeon/inflearn/NODE-API/api/user/user.spec.js
+++ JeongSeokHyeon/inflearn/NODE-API/api/user/user.spec.js
@@ -6,10 +6,10 @@ const should = require('should')
 const models = require('../../models')
 
 describe('GET /users는', () => {
+  const users = [{ name: 'alice' }, { name: 'bek' }, { name: 'chris' }]
+  before(() => models.sequelize.sync({ force: true }))
+  before(() => models.User.bulkCreate(users))
   describe('성공시', () => {
-    const users = [{ name: 'alice' }, { name: 'bek' }, { name: 'chris' }]
-    before(() => models.sequelize.sync({ force: true }))
-    before(() => models.User.bulkCreate(users))
     it('유저 객체를 담은 배열로 응답한다', (done) => {
       request(app)
         .get('/users')
@@ -34,6 +34,9 @@ describe('GET /users는', () => {
   })
 })
 describe('GET /users/:id', () => {
+  const users = [{ name: 'alice' }, { name: 'bek' }, { name: 'chris' }]
+  before(() => models.sequelize.sync({ force: true }))
+  before(() => models.User.bulkCreate(users))
   describe('성공시', () => {
     it('id가 1인 유저 객체를 반환한다', (done) => {
       request(app)
@@ -53,7 +56,10 @@ describe('GET /users/:id', () => {
     })
   })
 })
-describe.only('DELETE /users/:id', () => {
+describe('DELETE /users/:id', () => {
+  const users = [{ name: 'alice' }, { name: 'bek' }, { name: 'chris' }]
+  before(() => models.sequelize.sync({ force: true }))
+  before(() => models.User.bulkCreate(users))
   describe('성공시', () => {
     it('204를 응답한다', (done) => {
       request(app).delete('/users/1').expect(204).end(done)
@@ -65,7 +71,10 @@ describe.only('DELETE /users/:id', () => {
     })
   })
 })
-describe('POST /users', () => {
+describe.only('POST /users', () => {
+  const users = [{ name: 'alice' }, { name: 'bek' }, { name: 'chris' }]
+  before(() => models.sequelize.sync({ force: true }))
+  before(() => models.User.bulkCreate(users))
   describe('성공시', () => {
     let name = 'daniel',
       body
